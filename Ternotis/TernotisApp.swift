a524c061692024bbc6b58b18c6721c005f7731c0
Fauzan Ramadhana Sadikin <fauzanramadhana6@gmail.com>
2023-02-23 18:41:54 +0700

    Initial Commit

---
+++ Ternotis/TernotisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TernotisApp.swift
+//  Ternotis
+//
+//  Created by fauzan ramadhana on 23/02/23.
+//
+
+import SwiftUI
+
+@main
+struct TernotisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
